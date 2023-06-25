731e360412cd0d231057f0b2e9e8adf8c194994e
florianamette <50982737+florianamette@users.noreply.github.com>
2023-06-25 08:04:04 +0200

    Update ntlm_challenge_decoder.py

--- ntlm_challenge_decoder.py
+++ ntlm_challenge_decoder.py
@@ -108,9 +108,10 @@ target_field_types[5] = "Parent DNS domain"
 target_field_types[7] = "Server Timestamp"
 
 def main():
-    st_raw = sys.stdin.read()
+    st_raw = sys.argv[1]
     try:
         st = base64.b64decode(st_raw)
+        print(st)
     except e:
         print("Input is not a valid base64-encoded string")
         return
